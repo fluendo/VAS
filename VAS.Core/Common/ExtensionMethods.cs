e4e16d780052e7562d2ca84aa27b767d2ee31492
Axel Gorris <agorris@fluendo.com>
2018-01-23 15:47:28 +0100

    Add GetDescription extension method

--- VAS.Core/Common/ExtensionMethods.cs
+++ VAS.Core/Common/ExtensionMethods.cs
@@ -18,7 +18,9 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Dynamic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using VAS.Core.Events;
@@ -267,6 +269,33 @@ namespace VAS.Core.Common
 					return false;
 			}
 			return true;
+		}
+
+		/// <summary>
+		/// Returns the first Description attribute
+		/// </summary>
+		public static string GetDescription<T> (this T e) where T : IConvertible
+		{
+			string description = null;
+
+			if (e is Enum) {
+				Type type = e.GetType ();
+				Array values = System.Enum.GetValues (type);
+
+				foreach (int val in values) {
+					if (val == e.ToInt32 (CultureInfo.InvariantCulture)) {
+						var memInfo = type.GetMember (type.GetEnumName (val));
+						var descriptionAttributes = memInfo [0].GetCustomAttributes (typeof (DescriptionAttribute), false);
+						if (descriptionAttributes.Length > 0) {
+							// we're only getting the first description we find (others will be ignored)
+							description = ((DescriptionAttribute)descriptionAttributes [0]).Description;
+						}
+						break;
+					}
+				}
+			}
+
+			return description;
 		}
 	}
 }
